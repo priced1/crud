5991b2e569685dfefdee7183024ab02a61887782
G.J <priceddread13@gmail.com>
2023-06-07 20:31:35 -0300

    update e edit

--- src/main/java/br/com/belval/crud/controller/ProdutoController.java
+++ src/main/java/br/com/belval/crud/controller/ProdutoController.java
@@ -1,21 +1,19 @@
 package br.com.belval.crud.controller;
 
 import java.util.ArrayList;
-
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.belval.crud.model.Produto;
-import org.springframework.beans.factory.annotation.Autowired;
-import br.com.belval.crud.repository.ProdutoRepository;	
-
-import org.springframework.web.bind.annotation.PathVariable;
+import br.com.belval.crud.repository.ProdutoRepository;
 
 	
 
